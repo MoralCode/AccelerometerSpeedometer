78d71e6b6b6c06fe51d93da60f4a5b850a590cdf
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2016-10-16 13:14:12 -0700

    Added basic UI to display values

--- AccelerometerSpeedometer/ViewController.swift
+++ AccelerometerSpeedometer/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let speedo = AccelerometerSpeedometer()
+    var displayLink:CADisplayLink?
+    
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
 
+    
+    func update() {
+        print("1")
+        if xLabel != nil {xLabel.text = String(format: "x: %.02f", self.speedo.getAccelerationValues().x)}
+        if yLabel != nil {yLabel.text = String(format: "y: %.02f", self.speedo.getAccelerationValues().y)}
+        if zLabel != nil {zLabel.text = String(format: "z: %.02f", self.speedo.getAccelerationValues().z)}
+        
+        let total = abs(speedo.getAccelerationValues().x + speedo.getAccelerationValues().y + speedo.getAccelerationValues().z)
+        if totalLabel != nil {totalLabel.text = String(format: "total: %.02f", total)}
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("4")
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("3")
+        speedo.setupMotionManager()
+        
+        self.displayLink = CADisplayLink(target: self, selector: #selector(self.update))
+        if displayLink != nil {self.displayLink?.add(to: RunLoop.current, forMode: RunLoopMode.commonModes)} else {print("nil")}
+        print("2")
     }
 
     override func didReceiveMemoryWarning() {
